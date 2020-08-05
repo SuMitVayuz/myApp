a6ad4d0c95b8ce3d67e1de49e72b57e82486ca62
SuTechs <sumit123210@gmail.com>
2020-08-06 03:04:25 +0530

    theme to template

--- client/main.js
+++ client/main.js
@@ -1,33 +1,38 @@
 import {  Template} from 'meteor/templating';
-import {  HTTP} from 'meteor/http'
+
 import './main.html';
+import './test.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-
-});
-
-Template.hello.helpers({
-
-  result() {
-    return 'Su Mit';
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-
-    HTTP.post('https://leaderbridge.herokuapp.com/fetch_expertise', {
-      'headers': {
-        'Content-Type': 'application/json'
-      },
-    }, (error, result) => {
-      console.log('Error = ' + error);
-      console.log('Result = ' + result);
-
-    });
-
-
-
-
-  },
-});
+//
+// Template.hello.onCreated(function helloOnCreated() {
+// 
+// });
+//
+// Template.hello.helpers({
+//
+//   result() {
+//     return 'Su Mit';
+//   },
+// });
+//
+// Template.hello.events({
+//   'click button'(event, instance) {
+//
+//     console.log('Error = ');
+//
+//
+//     HTTP.post('https://leaderbridge.herokuapp.com/fetch_expertise', {
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//     }, (error, result) => {
+//       console.log('Error = ' + error);
+//       console.log('Result = ' + result);
+//
+//     });
+//
+//
+//
+//
+//   },
+// });
